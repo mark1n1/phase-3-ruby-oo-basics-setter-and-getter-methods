b87010c4c87c7c987577895ef5a8e0e300b5384d
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-07-17 12:36:18 -0700

    Using traditional getters and setters

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,20 @@
 class Person
-  attr_accessor :name
-  attr_accessor :job
+  # attr_accessor :name
+  # attr_accessor :job
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
 end
